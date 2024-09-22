38698835ed501b58124567b3e28ecf62f036d090
Артем Кочетков <60662226+derkochan@users.noreply.github.com>
2024-09-22 21:47:13 +0300

    Homework3.1

---
+++ Homework3.1.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int n;
+	int k;
+	float del;
+	printf("Введите число 1:");
+	scanf("%d", &n);
+	printf("Введите число 2:");
+	scanf("%d", &k);
+	del = k % n;
+	printf("+ = %d, - = %d, * = %d, / = %d, % = %f", \
+		k + n, k - n, k * n, k / n, del);
+	getchar();
+}
